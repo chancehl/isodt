3026a27444155705f50b069ce29b9da19ef3927f
Chance Linz <chanlinz@amazon.com>
2023-06-23 10:59:39 -0700

    📦 chore(Cargo.toml): add regex dependency 🚀 feat(main.rs): add support for date format "MM/DD/YYYY" and "MM/DD/YYYY HH:MM:SS" 📝 docs(args.rs): add accepted formats to Args struct documentation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use chrono::{DateTime, Local, TimeZone, Utc};
+use chrono::{NaiveDate, NaiveDateTime, NaiveTime, TimeZone, Utc};
 use clap::Parser;
 use models::args::Args;
+use regex::Regex;
 use std::process;
 
 mod models;
@@ -8,24 +9,42 @@ mod models;
 fn main() {
     let args = Args::parse();
 
-    let date_format = "%Y-%m-%d".to_string();
-    let date_time_format = "%Y-%m-%d %H:%M:%S".to_string();
+    let date_format = "%m/%d/%Y".to_string();
+    let date_regex = Regex::new("^\\d{2}\\/\\d{2}\\/\\d{4}$").unwrap();
+
+    let date_time_format = "%m/%d/%Y %H:%M:%S".to_string();
+    let date_time_regex =
+        Regex::new("^\\d{2}\\/\\d{2}\\/\\d{4}\\s{1}\\d{2}:\\d{2}:\\d{2}$").unwrap();
 
     if let Some(date) = &args.date {
-        println!(
-            "{:?}",
-            Utc.datetime_from_str(date, &date_format).expect(
-                &format!(
-                    "Could not convert date to ISO-8601 format. \
-                    Please use the {:?} format for dates and the {:?} format for dates with time.",
-                    date_format, date_time_format
-                )
-                .to_string()
+        if date_time_regex.is_match(date) {
+            println!(
+                "{:?}",
+                Utc.datetime_from_str(date, &date_time_format)
+                    .expect("Could not parse date time from date")
             )
-        )
-    } else {
-    };
+        } else if date_regex.is_match(date) {
+            let naive_date =
+                NaiveDate::parse_from_str(date, &date_format).expect("Could not parse date");
 
-    println!("{:?}", Utc::now());
-    process::exit(0);
+            let naive_time = NaiveTime::from_hms_opt(0, 0, 0)
+                .expect("Could not compute a naive time from hms values");
+
+            let naive_date_time = NaiveDateTime::new(naive_date, naive_time);
+
+            let fixed_offset = naive_date_time.and_utc().fixed_offset();
+
+            let utc = Utc
+                .from_local_datetime(&fixed_offset.naive_utc())
+                .single()
+                .expect("Could not compute UTC time from naively constructed date time");
+
+            println!("{:?}", utc);
+        } else {
+            panic!("Date {:?} did not match accepted formats. Please run \"isodt --help\" for a list of accepted formats", date);
+        }
+    } else {
+        println!("{:?}", Utc::now());
+        process::exit(0);
+    };
 }
--- src/models/args.rs
+++ src/models/args.rs
@@ -1,6 +1,9 @@
 use clap::Parser;
 
-/// Simple program to convert a date to an ISO-8601 string
+/// Simple program to convert a date to an ISO-8601 string.
+/// Accepted formats:
+/// "MM/DD/YYYY" ("01/01/1970"),
+/// "MM/DD/YYYY HH:MM:SS" ("01/01/1970 12:00:00")
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
